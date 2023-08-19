3a8d20aac7f8900c789fe63d92467aa2c6efa10f
lcomment <kohyunsuk98@gmail.com>
2023-08-19 15:49:16 +0900

    fix(#34): security config에 엔드포인트 추가

--- src/main/java/com/najasin/config/SecurityConfig.java
+++ src/main/java/com/najasin/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 		return requests ->
 			requests.requestMatchers(
 					new AntPathRequestMatcher("/swagger-ui/index.html"),
-					new AntPathRequestMatcher("/success"),
+					new AntPathRequestMatcher("/success/**"),
 					new AntPathRequestMatcher("/api/auth"),
 					new AntPathRequestMatcher("/auth2/**"),
 					new AntPathRequestMatcher("/login/**")
